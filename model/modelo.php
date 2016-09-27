532feb47ea8a755dbfd09648036a9f5daf1b6757
OswaldoCuzSimon <oswaldo_cs_94@hotmail.com>
2016-09-27 01:05:30 -0500

    cambio de estructura en la vista parte: 2

--- model/modelo.php
+++ model/modelo.php
@@ -2,7 +2,8 @@
 class Modelo{
 	public function addProfesor($profesor,$usuario_id){
 		include ("config.php");
-		
+		$result = false;
+		$stmt = null;
 		if ($stmt = $conn->prepare("INSERT INTO profesores (profesor_id,usuario_id,nombre,carga_academica)
 			VALUES(?,?,?,?);")) {
 
@@ -13,14 +14,21 @@ class Modelo{
 			echo "<p>".$profesor->getProfesor_id().$usuario_id.$profesor->getNombre().$profesor->getCargaAcademica()."</p>";
 			$result = $stmt->execute();
 
-			if($result){
-				return true;
-			}else{
-				return false;
+			if(!$result){
+				error_log(sprintf("[%d] %s",$stmt->sqlstate,$stmt->error));
 			}
+			
+		}else{
+			error_log(sprintf("[%d] %s",$stmt->sqlstate,$stmt->error));
 		}
+
+		
+		
 		$stmt->close();
-		$conn->close();
+		$conn->close();	
+
+		return $result;
+		
 	}
 	public function addUEA($uea,$usuario_id,$solucion_id){
 		include ("config.php");
@@ -28,28 +36,41 @@ class Modelo{
 		
 		if ($stmt = $conn->prepare("INSERT INTO UEA (uea_id,usuario_id,nombre,grupo,cupo,horas,nivel,trimestre,obligatoria,creditos)
 			VALUES(?,?,?,?,?,?,?,?,?);")) {
-			$stmt->bind_param(1,$uea->getUea_id());
-			$stmt->bind_param(2,$uea->getNombre());
-			$stmt->bind_param(3,$uea->getGrupo());
-			$stmt->bind_param(4,$uea->getCupo());
-			$stmt->bind_param(5,$uea->getHoras());
-			$stmt->bind_param(6,$uea->getNivel());
-			$stmt->bind_param(7,$uea->getTrimestre());
-			$stmt->bind_param(8,$uea->getObligatoria());
-			$stmt->bind_param(9,$uea->getCreditos());
+			$uea_id 	= $uea->getUea_id();
+			$nombre 	= $uea->getNombre();
+			$grupo 		= $uea->getGrupo();
+			$cupo 		= $uea->getCupo();
+			$horas 		= $uea->getHoras();
+			$nivel 		= $uea->getNivel();
+			$trimestre 	= $uea->getTrimestre();
+			$obligatoria = $uea->getObligatoria();
+			$creditos 	= $uea->getCreditos();
 
-			$stmt->execute();
+			$stmt->bind_param(1,$uea_id);
+			$stmt->bind_param(2,$usuario_id);
+			$stmt->bind_param(3,$nombre);
+			$stmt->bind_param(4,$grupo);
+			$stmt->bind_param(5,$cupo);
+			$stmt->bind_param(6,$horas);
+			$stmt->bind_param(7,$nivel);
+			$stmt->bind_param(8,$trimestre);
+			$stmt->bind_param(9,$obligatoria);
+			$stmt->bind_param(10,$creditos);
+
+			$result = $stmt->execute();
+		}else{
+			$stmt->close();
+			$conn->close();
+			$result = false;
 		}
-		$stmt->close();
-		$conn->close();
+		return $result;
 
 	}
 	public function deleteProfesor($profesor_id,$usuario_id,$solucion_id){
 		include ("config.php");
-		if ($stmt = $conn->prepare("DELETE FROM profesores WHERE usuario_id=? and profesor_id=? and solucion_id=?;")) {
+		if ($stmt = $conn->prepare("DELETE FROM profesores WHERE usuario_id=? and profesor_id=?;")) {
 			$stmt->bind_param(1,$usuario_id);
 			$stmt->bind_param(2,$profesor_id);
-			$stmt->bind_param(1,$solucion_id);
 
 			$stmt->execute();
 		}
@@ -68,8 +89,27 @@ class Modelo{
 		$stmt->close();
 		$conn->close();
 	}
-	public function updateProfesor($uea,$usuario_id,$solucion_id){
+	public function updateProfesor($profesor,$usuario_id,$solucion_id){
+		include ("config.php");
+		
+		if ($stmt = $conn->prepare("UPDATE profesores SET (profesor_id,usuario_id,nombre,carga_academica)
+			WHERE profesor_id=? AND usuario_id = ?;")) {
 
+			$profesor_id = $profesor->getProfesor_id();
+			$nombre = $profesor->getNombre();
+			$carga_academica = $profesor->getCargaAcademica();
+			$stmt->bind_param('iisi',$profesor_id,$usuario_id,$nombre,$carga_academica);
+			echo "<p>".$profesor->getProfesor_id().$usuario_id.$profesor->getNombre().$profesor->getCargaAcademica()."</p>";
+			$result = $stmt->execute();
+
+			if($result){
+				return true;
+			}else{
+				return false;
+			}
+		}
+		$stmt->close();
+		$conn->close();
 	}
 	public function updateUEA($usuario_id,$solucion_id){
 
